0e7d085c152c2a44df225dab9d87d9aec1e3250a
Patrik <patri.moravec@email.cz>
2023-12-19 13:25:46 +0100

    přidání editace todolist

--- js.js
+++ js.js
@@ -2,12 +2,25 @@ function addTask() {
     let taskInput = document.getElementById("taskInput");
     let taskList = document.getElementById("taskList");
     let newTask = document.createElement("div");
-    newTask.className="p"
-    newTask.textContent = taskInput.value;
+    newTask.className = "task";
+    let taskText = document.createElement("input");
+    taskText.type = "text";
+    taskText.value = taskInput.value;
+    taskText.disabled = true;
+    newTask.appendChild(taskText);
+
+    let editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.className = "edit";
+    editButton.onclick = function () {
+        taskText.disabled = !taskText.disabled;
+    };
+    newTask.appendChild(editButton);
+
     let removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.className = "remove";
-    removeButton.onclick = function() {
+    removeButton.onclick = function () {
         taskList.removeChild(newTask);
     };
     newTask.appendChild(removeButton);
